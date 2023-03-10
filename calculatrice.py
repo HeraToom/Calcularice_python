a87bbab631c21a8df833431e6997d1e06dfd4560
Ryan <k.ryan.77@hotmail.fr>
2023-03-10 18:38:40 +0100

    commit debut projet calculatrice - commiting all files to the repository git status

---
+++ calculatrice.py
@@ -0,0 +1,8 @@
+#demander le premier nombre
+a = input("Entrez un premier nombre: ")
+#demander le deuxieme nombre
+b = input("Entrez un deuxième nombre: ")
+#faire le calcule
+c = int(a) + int(b)
+#afficher le resultat
+print(f"Le résultat de l'addition de {a} avec {b} est égal à {c}")
